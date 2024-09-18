1edf5a6cd6335a4ec51f1bb110a720d4c01cb352
AndreasNelson <nel02254@morris.umn.edu>
2024-09-18 15:58:03 -0500

    added comments to main

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+// CSCI 3501 Lab 4
+// Andreas Nelson and Nate Ferrell
+
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
